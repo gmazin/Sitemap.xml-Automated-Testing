37b1a1e8e8e31cd6acc5849450336e631c632133
Georgiy Mazin <gmazin@georgiymaz.in>
2013-04-19 16:47:43 -0500

    Stability improvements

--- sitemap_xml_testing.js
+++ sitemap_xml_testing.js
@@ -1,30 +1,3 @@
-/*
-Sitemap.xml Automated Testing Script using CasperJS
-
-Usage instructions: casperjs sitemap_xml_testing.js --sitemap=<URL TO SITEMAP> --logfile=<LOG FILE NAME>
-
-This script will attempt to crawl through a specified sitemap to check children pages for broken urls, 
-images, css, and Javascript. Errors will be recorded to the logfile specified. This script requires 
-casperjs to be installed.
-
-Copyright (c) 2013 Georgiy Mazin
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or 
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 var links = [];
 var intlinks = [];
 var intimages = [];
@@ -122,11 +95,17 @@ casper.on('http.status.504', function(resource) {
 casper.on("page.error", function(msg, trace) {
   this.echo("Error:    " + msg, "ERROR");
   fs.write(logfile, "Error:    " + msg + "\n", 'a'); 
-  this.echo("file:     " + trace[0].file, "WARNING");
+  if (trace[0].file){
+    this.echo("file:     " + trace[0].file, "WARNING");
+  }
+  if (trace[0].line){
   fs.write(logfile, "file:     " + trace[0].file + "\n", 'a'); 
-  this.echo("line:     " + trace[0].line, "WARNING");
-  fs.write(logfile, "line:     " + trace[0].line + "\n", 'a'); 
-  this.echo("function: " + trace[0]["function"], "WARNING");
+    this.echo("line:     " + trace[0].line, "WARNING");
+  }
+  if (trace[0]["function"]){
+    fs.write(logfile, "line:     " + trace[0].line + "\n", 'a'); 
+        this.echo("function: " + trace[0]["function"], "WARNING");
+    }
   fs.write(logfile, "function: " + trace[0]["function"] + "\n", 'a'); 
   msg = 0;
   trace = [];
@@ -134,6 +113,7 @@ casper.on("page.error", function(msg, trace) {
 
 
 casper.start(sitemap, function() {
+    this.echo(logfile);
     fs.write(logfile, 'Starting crawl...\n', 'w'); 
     links = this.evaluate(getLinks);
 });
